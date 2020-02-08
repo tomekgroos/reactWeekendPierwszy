9766f981b8efee37cae6ee4e441d084b5cc741b1
tomekgroos <tomek.groos@gmail.com>
2020-02-08 15:43:46 +0100

    8 luty

--- first-homework-app/src/cwiczenie1/cw1.js
+++ first-homework-app/src/cwiczenie1/cw1.js
@@ -1,26 +1,35 @@
 import React from "react";
 
+
 export default class Person extends React.Component{
     constructor(props) {
         super(props);
 
         this.state = {
-           name: "Tomek",
-           surname: "Groos",
-           age: 30,
-           sex: "m"
+           name: "",
+           surname: "",
+           age: null,
+           sex: ""
         };
     }
 
-    
+    changeHandler = (e) => {
+
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+
+
 
     render() {
-        return (
+        return(
             <div>
-              
+
+                <input value={this.state.name} label="imie" onChange={this.changeHandler}/>
+                <h1>{this.state.name}</h1>
+
             </div>
         )
-    };
-
+    }
 
 }
