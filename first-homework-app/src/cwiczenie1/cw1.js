5c38768b5ba4f8bafef703f38381ee3f6886d34f
Your Name <you@example.com>
2020-02-08 22:42:27 +0100

    proba cw1

--- first-homework-app/src/cwiczenie1/cw1.js
+++ first-homework-app/src/cwiczenie1/cw1.js
@@ -1,35 +1,101 @@
-import React from "react";
+import React from 'react';
 
-
-export default class Person extends React.Component{
+export default class MyForm extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
-           name: "",
-           surname: "",
-           age: null,
-           sex: ""
+            username: '',
+            surname: '',
+            age: null,
+            sex: "",
+            errormessage: ''
         };
     }
+    myChangeHandler = (event) => {
+        let nam = event.target.name;
+        let val = event.target.value;
+        let errAge = '';
+        let errName = '';
+        let errSurname = '';
+        let errSex = '';
+        let errCapital = '';
 
-    changeHandler = (e) => {
 
-        this.setState({[e.target.name]: e.target.value});
+        if (nam === "age") {
+            if (val !="" && !Number(val)) {
+                errAge = <strong>Your age must be a number</strong>;
+
+            }
+            this.setState({errormessage: errAge});
+        }
+        
+
+        if (nam == "username"){
+            if (val.length <= 2){
+                errName = <strong>Your name must be more than 2 characters</strong>;
+            }  
+            this.setState({errormessage: errName});
+        }
+
+        if (nam == "sex"){
+            if (val !== "m" || val !=="k"){
+                errSex = <strong>To twoj moment! Zdecyduj czy jestes kobieta "k",
+                    czy mezczyzna "m"
+                </strong>;
+            }  
+            this.setState({errormessage: errSex});
+        }
+
+        if (nam == "surname"){
+            if (val.length <= 2){
+                errSurname = <strong>Your surname must be more than 2 characters</strong>;
+            }  
+            this.setState({errormessage: errSurname});
+        }
+
+        if (nam == "username" || nam == "surname") {
+            if (val != "[A-Z]{1}[^\s]+"){
+                errCapital = <strong>Please begin with Capital letter</strong>
+            }
+            this.setState({errormessage: errCapital});
+        }
+        
+
+        this.setState({[nam]: val});
     }
-
-
-
 
     render() {
-        return(
-            <div>
+        return (
+            <form>
+                <h1>Hello {this.state.username} {this.state.age}{this.state.surname} of Sex {this.state.sex}</h1>
+                <p>Enter your name:</p>
+                <input
+                    type='text'
+                    name='username'
+                    onChange={this.myChangeHandler}
+                />
+                <p>Enter your surname:</p>
+                <input
+                    type='text'
+                    name='surname'
+                    onChange={this.myChangeHandler}
+                />
+                <p>Enter your age:</p>
+                <input
+                    type='text'
+                    name='age'
+                    onChange={this.myChangeHandler}
+                />
 
-                <input value={this.state.name} label="imie" onChange={this.changeHandler}/>
-                <h1>{this.state.name}</h1>
+                <p>Enter your sex:</p>
+                <input
+                    type='text'
+                    name='sex'
+                    onChange={this.myChangeHandler}
+                />
 
-            </div>
-        )
+               <br/> <br/> {this.state.errormessage}
+            </form>
+        );
     }
-
 }
